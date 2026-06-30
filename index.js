68cc61dd91daf2aa66cc6247737b330ed7f096c3
IamFreak10 <mahfujabdulla9@gmail.com>
2026-06-30 16:28:17 +0600

    Setuped Schema

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
-
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
