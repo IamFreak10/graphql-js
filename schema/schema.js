b777c02397bfcf2fb5fba96d961d20c85b4a55fb
IamFreak10 <mahfujabdulla9@gmail.com>
2026-07-01 12:35:10 +0600

    Added Mutaions

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,37 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} = require('graphql');
+const users = [
+  { id: '1', name: 'A', email: 'a@b.com', age: 20 },
+  { id: '2', name: 'B', email: 'b@b.com', age: 30 },
+  { id: '3', name: 'C', email: 'c@b.com', age: 40 },
+];
+const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    user: {
+      type: userType,
+      args: {
+        id: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return users.find((user) => user.id === args.id);
+      },
+    },
+
     hello: {
       type: GraphQLString,
       resolve() {
@@ -12,6 +41,30 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const Mutaions = new GraphQLObjectType({
+  name: 'Mutaions',
+  fields: {
+    addUser: {
+      type: userType,
+      args: {
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        const newUser = {
+          id: users.length + 1,
+          name: args.name,
+          email: args.email,
+          age: args.age,
+        };
+        users.push(newUser);
+        return newUser;
+      },
+    },
+  },
+});
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutaions,
 });
